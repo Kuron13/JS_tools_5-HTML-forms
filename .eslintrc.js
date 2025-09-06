6f5002597cd95ded9de3f4805e6ebce2e01d8d6b
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-09-07 01:33:01 +0500

    e2e

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    jest: true,
+  },
+  globals: {
+    page: true,
+    browser: true,
+    context: true,
+    puppeteerConfig: true,
+    jestPuppeteer: true,
+  },
+};
