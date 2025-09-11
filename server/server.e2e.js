615461caab8b5a87087517e2209b2bdf7df30842
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-09-11 13:36:08 +0500

    Update server.e2e.js

--- server/server.e2e.js
+++ server/server.e2e.js
@@ -1,9 +1,9 @@
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
-const config:... = require("..webpack.dev");
+const config = require("..webpack.dev");
 
 const server = new WebpackDevServer(webpack(config), {});
-server.listen(8080, "localhost", (err):void => {
+server.listen(8080, "localhost", (err) => {
   if (err) {
     return;
   }
